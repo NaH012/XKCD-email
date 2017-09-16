31a9f9cecf75b970cb1f0860bf4251a0ebb9a126
Michael Julander <1110mdj@gmail.com>
2017-09-16 14:46:44 -0600

    Update emailXKCD.sh

--- emailXKCD.sh
+++ emailXKCD.sh
@@ -1,23 +1,22 @@
 sent="false"
-mkdir $dir/xkcd
-dir=$dir/xkcd
+mkdir $dir/tmp
 touch $dir/viewed.pictures
-wget -O $dir/index.html xkcd.com/index.html
+wget -O $dir/tmp/index.html xkcd.com/index.html
 while [ $sent = "false" ]
 do
-	num="$(cat $dir/index.html | grep Permanent\ link | cut -d'/' -f4)"
-	img="$(cat $dir/index.html | grep -A 1 \<div\ id=\"comic\" | grep -v \<div)"
+	num="$(cat $dir/tmp/index.html | grep Permanent\ link | cut -d'/' -f4)"
+	img="$(cat $dir/tmp/index.html | grep -A 1 \<div\ id=\"comic\" | grep -v \<div)"
 	url="$(echo "{$img}" | cut -d'/' -f3- | cut -d'"' -f1)"
 	name="$(echo $url | cut -d'/' -f3)"
 	alt="$(echo "{$img}" | cut -d'"' -f4 | recode html..ascii)"
 	used="$(cat $dir/viewed.pictures | grep $num)"
 	if [ -z "$used" ]; then
-		wget -O $dir/$name $url
+		wget -O $dir/tmp/$name $url
 		echo $num >> $dir/viewed.pictures
-		rm $dir/index.html*
-		file="$(ls -l $dir | grep - | grep -v -E 'script.sh|viewed.pictures' | sed 's/  */ /g' | cut -d' ' -f9)"
-		echo $alt | mail -A $dir/$file $*
-		rm $dir/$file
+		rm $dir/tmp/index.html*
+		file="$(ls -l $dir/tmp | grep - | sed 's/  */ /g' | cut -d' ' -f9)"
+		echo $alt | mail -A $dir/tmp/$file $*
+		rm -r $dir/tmp
 		sent="true"
 	else
 		while [ -n "$used" ]
@@ -25,7 +24,7 @@ do
 			new="$(shuf -i 1-$num -n 1)"
 			used="$(cat $dir/viewed.pictures | grep $new)"
 		done
-		rm $dir/index.html*
-		wget -O $dir/index.html xkcd.com/$new/index.html
+		rm $dir/tmp/index.html*
+		wget -O $dir/tmp/index.html xkcd.com/$new/index.html
 	fi
 done

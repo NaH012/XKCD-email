5c555e0b45f94f1c2f7b1cb25a388d0636574b58
NaH012 <1110mdj@gmail.com>
2018-06-21 19:02:11 -0600

    Make wget silent

--- emailXKCD.sh
+++ emailXKCD.sh
@@ -5,7 +5,7 @@ dir=$(dirname $dir)
 sent="false"
 mkdir $dir/tmp
 touch $dir/viewed.pictures
-wget -O $dir/tmp/index.html xkcd.com/index.html
+wget -q -O $dir/tmp/index.html xkcd.com/index.html
 while [ $sent = "false" ]
 do
 	num="$(cat $dir/tmp/index.html | grep Permanent\ link | cut -d'/' -f4)"
@@ -15,7 +15,7 @@ do
 	alt="$(echo "{$img}" | cut -d'"' -f4 | recode html..ascii)"
 	used="$(cat $dir/viewed.pictures | grep $num)"
 	if [ -z "$used" ]; then
-		wget -O $dir/tmp/$name $url
+		wget -q -O $dir/tmp/$name $url
 		echo $num >> $dir/viewed.pictures
 		rm $dir/tmp/index.html*
 		file="$(ls -l $dir/tmp | grep - | sed 's/  */ /g' | cut -d' ' -f9)"
@@ -29,6 +29,6 @@ do
 			used="$(cat $dir/viewed.pictures | grep $new)"
 		done
 		rm $dir/tmp/index.html*
-		wget -O $dir/tmp/index.html xkcd.com/$new/index.html
+		wget -q -O $dir/tmp/index.html xkcd.com/$new/index.html
 	fi
 done
